830ba879c4209553840338d390ec3c2a5e2c569f
Arnar Gauti Ingason <arnarg@fastmail.com>
2024-05-03 13:03:52 +0200

    Add transformer to remove selected helm labels

--- modules/default.nix
+++ modules/default.nix
@@ -23,6 +23,17 @@
     nixidy.defaults = {
       syncPolicy.automated.selfHeal = true;
       syncPolicy.automated.prune = true;
+
+      # Many helm chars will render all resources with the
+      # following labels.
+      # This produces huge diffs when the charts are updated
+      # because the values of these labels change each release.
+      # Here we add a transformer that strips them out after
+      # templating the helm charts in each application.
+      helm.transformer = map (lib.kube.removeLabels [
+        "app.kubernetes.io/version"
+        "helm.sh/chart"
+      ]);
     };
   };
 }
