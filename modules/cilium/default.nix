65d209d4732db549f690bb2310da09dae53af4b6
Arnar Gauti Ingason <arnarg@fastmail.com>
2024-05-03 14:39:18 +0200

    Add cilium system wide network policies

--- modules/cilium/default.nix
+++ modules/cilium/default.nix
@@ -68,7 +68,155 @@ in {
         inherit chart values;
       };
 
-      # TODO: add network policies
+      yamls = [
+        ''
+          apiVersion: "cilium.io/v2"
+          kind: CiliumClusterwideNetworkPolicy
+          metadata:
+            name: allow-internal-egress
+            namespace: kube-system
+          spec:
+            description: "Policy to allow all Cilium managed endpoint to talk to all other cilium managed endpoints on egress"
+            endpointSelector: {}
+            egress:
+            - toEndpoints:
+              - {}
+        ''
+        ''
+          apiVersion: "cilium.io/v2"
+          kind: CiliumClusterwideNetworkPolicy
+          metadata:
+            name: "cilium-health-checks"
+            namespace: kube-system
+          spec:
+            endpointSelector:
+              matchLabels:
+                'reserved:health': ""
+            ingress:
+              - fromEntities:
+                - remote-node
+            egress:
+              - toEntities:
+                - remote-node
+        ''
+        ''
+          apiVersion: "cilium.io/v2"
+          kind: CiliumNetworkPolicy
+          metadata:
+            name: allow-hubble-relay-server-egress
+            namespace: kube-system
+          spec:
+            description: "Policy for egress from hubble relay to hubble server in Cilium agent."
+            endpointSelector:
+              matchLabels:
+                app.kubernetes.io/name: hubble-relay
+            egress:
+            - toEntities:
+              - remote-node
+              - host
+              toPorts:
+              - ports:
+                - port: "4244"
+                  protocol: TCP
+        ''
+        ''
+          apiVersion: "cilium.io/v2"
+          kind: CiliumNetworkPolicy
+          metadata:
+            name: allow-hubble-ui-relay-ingress
+            namespace: kube-system
+          spec:
+            description: "Policy for ingress from hubble UI to hubble relay."
+            endpointSelector:
+              matchLabels:
+                app.kubernetes.io/name: hubble-relay
+            ingress:
+            - fromEndpoints:
+              - matchLabels:
+                  app.kubernetes.io/name: hubble-ui
+              toPorts:
+              - ports:
+                - port: "4245"
+                  protocol: TCP
+        ''
+        ''
+          apiVersion: cilium.io/v2
+          kind: CiliumNetworkPolicy
+          metadata:
+            name: allow-hubble-ui-kube-apiserver-egress
+            namespace: kube-system
+          spec:
+            description: "Allow Hubble UI to talk to kube-apiserver"
+            endpointSelector:
+              matchLabels:
+                app.kubernetes.io/name: hubble-ui
+            egress:
+            - toEntities:
+              - kube-apiserver
+              toPorts:
+              - ports:
+                - port: "6443"
+                  protocol: TCP
+        ''
+        ''
+          apiVersion: "cilium.io/v2"
+          kind: CiliumClusterwideNetworkPolicy
+          metadata:
+            name: allow-kube-dns-cluster-ingress
+            namespace: kube-system
+          spec:
+            description: "Policy for ingress allow to kube-dns from all Cilium managed endpoints in the cluster"
+            endpointSelector:
+              matchLabels:
+                k8s:io.kubernetes.pod.namespace: kube-system
+                k8s-app: kube-dns
+            ingress:
+            - fromEndpoints:
+              - {}
+              toPorts:
+              - ports:
+                - port: "53"
+                  protocol: UDP
+        ''
+        ''
+          apiVersion: "cilium.io/v2"
+          kind: CiliumNetworkPolicy
+          metadata:
+            name: allow-kube-dns-upstream-egress
+            namespace: kube-system
+          spec:
+            description: "Policy for egress to allow kube-dns to talk to upstream DNS."
+            endpointSelector:
+              matchLabels:
+                k8s-app: kube-dns
+            egress:
+            - toEntities:
+              - world
+              toPorts:
+              - ports:
+                - port: "53"
+                  protocol: UDP
+        ''
+        ''
+          apiVersion: "cilium.io/v2"
+          kind: CiliumNetworkPolicy
+          metadata:
+            name: allow-kube-dns-apiserver-egress
+            namespace: kube-system
+          spec:
+            description: "Allow coredns to talk to kube-apiserver"
+            endpointSelector:
+              matchLabels:
+                k8s-app: kube-dns
+            egress:
+            - toEntities:
+              - kube-apiserver
+              toPorts:
+              - ports:
+                - port: "6443"
+                  protocol: TCP
+        ''
+      ];
     };
 
     # Set resource exclusions in argocd
