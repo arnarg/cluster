0f3a1eabfd29fc9a14f8a0a13e62dc3b749f1c75
Arnar Gauti Ingason <arnarg@fastmail.com>
2024-05-24 14:06:43 +0200

    Update ArgoCD

--- modules/argocd/default.nix
+++ modules/argocd/default.nix
@@ -8,8 +8,8 @@
   chart = lib.helm.downloadHelmChart {
     repo = "https://argoproj.github.io/argo-helm/";
     chart = "argo-cd";
-    version = "6.7.18";
-    chartHash = "sha256-FWk0b9QyxC2dXuB9uK6A5OO999JghFucS4iOu2QNNNY=";
+    version = "6.11.1";
+    chartHash = "sha256-jyTfFYQipr2kzRIQxpPIupMiwu8qcQSn3Y5c7eS320I=";
   };
 
   namespace = "argocd";
