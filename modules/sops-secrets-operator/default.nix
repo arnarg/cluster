fe93b4ebc46dc31e9d30f8d72699eafa1dfccaa8
Arnar Gauti Ingason <arnarg@fastmail.com>
2024-05-24 14:13:59 +0200

    Update sops-secrets-operator

--- modules/sops-secrets-operator/default.nix
+++ modules/sops-secrets-operator/default.nix
@@ -2,8 +2,8 @@
   chart = lib.helm.downloadHelmChart {
     repo = "https://isindir.github.io/sops-secrets-operator/";
     chart = "sops-secrets-operator";
-    version = "0.17.4";
-    chartHash = "sha256-gkWySGFVuF4GOFExbJPu4W4KU9YBZcfh1U3cJBjvVVo=";
+    version = "0.19.0";
+    chartHash = "sha256-90b7Q2hJ91EDrwNJv0vY6iIfztdhLnur0i5SBJCTjXQ=";
   };
 
   namespace = "sops";
