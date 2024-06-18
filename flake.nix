99bae7f1a4536b4030926dc49802be01ff95064e
Arnar Gauti Ingason <arnarg@fastmail.com>
2024-06-18 16:48:17 +0200

    Move nixidy back to main

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   inputs.nixidy = {
-    url = "github:arnarg/nixidy/typed";
+    url = "github:arnarg/nixidy";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
