716dbf2573b8c9ae8dae6709645695557a6f2bbb
Arnar Gauti Ingason <arnarg@fastmail.com>
2024-06-18 16:28:59 +0200

    Update traefik helm chart

--- charts/traefik/traefik/default.nix
+++ charts/traefik/traefik/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://traefik.github.io/charts/";
   chart = "traefik";
-  version = "28.2.0";
-  chartHash = "sha256-+aDcAeuCaFw3Bms8oauSu/CBClHyNWK+y8seQyRfMa8=";
+  version = "28.3.0";
+  chartHash = "sha256-wRo7HRQpYF8Om6EwcOd7Krc12uJgElYgrY/xN1j0UNk=";
 }
