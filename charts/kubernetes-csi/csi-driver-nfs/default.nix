fd70a61fed52b302cfc5d2791aedb6f78fd8ebcd
Arnar Gauti Ingason <arnarg@fastmail.com>
2024-06-28 13:15:21 +0200

    Fix helm charts

--- charts/kubernetes-csi/csi-driver-nfs/default.nix
+++ charts/kubernetes-csi/csi-driver-nfs/default.nix
@@ -2,5 +2,5 @@
   repo = "https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts";
   chart = "csi-driver-nfs";
   version = "4.7.0";
-  chartHash = "sha256-HXj2NwivxhMpOXwXuj/9VMqy/MQyVYPdKC3bSj90VIc=";
+  chartHash = "sha256-EU2qaZglUU3vxa41l1p/2yBscksIhYMr8kSgH8t0vL8=";
 }
