ff558f1b70d8e1a542c07390fc242a363c4a2535
Arnar Gauti Ingason <arnarg@fastmail.com>
2024-08-29 11:57:15 +0200

    Update csi-driver-nfs chartHash

--- charts/kubernetes-csi/csi-driver-nfs/default.nix
+++ charts/kubernetes-csi/csi-driver-nfs/default.nix
@@ -2,5 +2,5 @@
   repo = "https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts";
   chart = "csi-driver-nfs";
   version = "4.8.0";
-  chartHash = "sha256-8wsqqvjkUGKD1n34jWNlHYqXu+ZVDz/aPHUpd7WvPms=";
+  chartHash = "sha256-UrsAoAurfH2hkLxgSgPcshqORHRxJJ1PZ4SnL7ZYsb8=";
 }
