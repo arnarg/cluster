90ec7aa75abcebf272b7978cb0f2e324980b1e1e
Arnar Gauti Ingason <arnarg@fastmail.com>
2025-02-28 19:20:14 +0100

    Update LLM review system prompt

--- .github/llm-review/main.py
+++ .github/llm-review/main.py
@@ -1,3 +1,4 @@
+import pathlib
 import sys
 import os
 import re
@@ -5,28 +6,12 @@ import openai
 
 BASE_URL = "https://api.hyperbolic.xyz/v1"
 
-SYSTEM_PROMPT = """
-You're reviewing a pull request change.
-Point out any potential issues that could arise from this changed being merged.
-Do not assume that anyone has verified that the changes work.
-In case of version changes, try to include a link to release notes.
-You're only concerned with changes to Kubernetes manifests in the `manifests/` folder.
-Don't ask any questions.
-"""
 
+def get_sys_prompt() -> str:
+    dir = pathlib.Path(__file__).parent.resolve()
 
-def parse_content(content: str):
-    regex = r"<think>(.*)<\/think>(.*)"
-    m = re.findall(regex, content, flags=re.DOTALL)
-    if m and m[0]:
-        thinking = m[0][0].strip()
-        response = m[0][1].strip()
-        return {
-            "thinking": thinking,
-            "response": response,
-        }
-    else:
-        raise Exception(f"Could not parse the response: {content}")
+    with open(f"{dir}/prompt.md", "r") as stream:
+        return stream.read()
 
 
 def do_completion(api_key: str, system: str, prompt: str):
@@ -53,23 +38,7 @@ def do_completion(api_key: str, system: str, prompt: str):
 
     response = completion.choices[0].message.content
 
-    return parse_content(response)
-
-
-def do_review(api_key: str, diff: str):
-    PROMPT = f"""```diff
-{diff}
-```"""
-
-    content = do_completion(api_key, SYSTEM_PROMPT, PROMPT)
-
-    return f"""{content["response"]}
-
-<details>
-  <summary>My thinking</summary>
-
-  > {'  > '.join(content["thinking"].splitlines(True))}
-</details>"""
+    return response
 
 
 def print_result(result: str):
@@ -90,6 +59,8 @@ if __name__ == "__main__":
 
     diff = sys.stdin.read()
 
-    md = do_review(api_key, diff)
+    sys_prompt = get_sys_prompt()
+
+    md = do_completion(api_key, sys_prompt, diff)
 
     print_result(md)
