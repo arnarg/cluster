e5033279fee473d10c948d2e130207f5f6e5e6a3
Arnar Gauti Ingason <arnarg@fastmail.com>
2025-11-19 20:13:33 +0100

    chore: remove unused external service

--- modules/external/default.nix
+++ modules/external/default.nix
@@ -8,53 +8,6 @@ in
     createNamespace = true;
 
     resources = {
-      services.lab.spec = {
-        ports = [
-          {
-            name = "https";
-            port = 443;
-            targetPort = 443;
-          }
-        ];
-      };
-
-      endpointSlices.lab = {
-        metadata.labels."kubernetes.io/service-name" = "lab";
-        addressType = "IPv4";
-        endpoints = [
-          {
-            addresses = [ "192.168.0.10" ];
-            conditions.ready = true;
-          }
-        ];
-        ports = [
-          {
-            name = "https";
-            port = 443;
-          }
-        ];
-      };
-
-      ingresses.lab.spec = {
-        inherit (config.networking.traefik) ingressClassName;
-
-        rules = [
-          {
-            host = "lab.${config.networking.domain}";
-            http.paths = [
-              {
-                path = "/";
-                pathType = "Prefix";
-                backend.service = {
-                  name = "lab";
-                  port.name = "https";
-                };
-              }
-            ];
-          }
-        ];
-      };
-
       services.tm.spec.ports = [
         {
           name = "http";
