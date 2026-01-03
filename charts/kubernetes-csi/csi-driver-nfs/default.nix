ac78e57e516cbb7c22842c68cc981d68b19b2e36
Arnar <arnar@codedbearder.com>
2026-01-03 11:20:16 +0100

    refactor: remove nfs-csi-driver chart in favour of nixhelm

--- charts/kubernetes-csi/csi-driver-nfs/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  repo = "https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/master/charts";
-  chart = "csi-driver-nfs";
-  version = "4.9.0";
-  chartHash = "sha256-+j73xTgdOAMZS6TZUP5RaSRfJJ8b98TeyR2zgKPKL4c=";
-}
